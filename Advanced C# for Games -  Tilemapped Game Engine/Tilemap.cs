656708101d32edd223f404d3af8da92d0d81f1da
Mansteel555 <denib79@gmail.com>
2024-02-28 15:03:56 +0200

    Basic Tilemap Added

---
+++ Advanced C# for Games -  Tilemapped Game Engine/Tilemap.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advanced_C__for_Games____Tilemapped_Game_Engine
+{
+    internal class Tilemap
+    {
+        private int[,] tiles;
+        private int width;
+        private int height;
+
+        public Tilemap(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+            tiles = new int[height, width]; 
+        }
+
+        public int GetTile(int x, int y)
+        {
+            if (IsValidPosition(x, y))
+            {
+                return tiles[y, x];
+            }
+            else
+            {
+                
+                return -1; 
+            }
+        }
+
+        public void SetTile(int x, int y, int tileValue)
+        {
+            if (IsValidPosition(x, y))
+            {
+                tiles[y, x] = tileValue;
+            }
+            else
+            {
+                
+            }
+        }
+
+        public void PrintTilemap()
+        {
+            for (int row = 0; row < height; row++)
+            {
+                for (int col = 0; col < width; col++)
+                {
+                    Console.Write(tiles[row, col] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private bool IsValidPosition(int x, int y)
+        {
+            return x >= 0 && x < width && y >= 0 && y < height;
+        }
+    }
+
+}
+}
--- Advanced C# for Games -  Tilemapped Game Engine/obj/Release/net8.0/Advanced C# for Games -  Tilemapped Game Engine.AssemblyInfo.cs
+++ Advanced C# for Games -  Tilemapped Game Engine/obj/Release/net8.0/Advanced C# for Games -  Tilemapped Game Engine.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Advanced C# for Games -  Tilemapped Game Engine")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Advanced C# for Games -  Tilemapped Game Engine")]
