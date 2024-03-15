c474d99f0e4ce668a070e1fab238881a7e5828f0
Mansteel555 <denib79@gmail.com>
2024-03-15 21:20:23 +0200

    Added changes to the tilemap class

--- Advanced C# for Games -  Tilemapped Game Engine/Tilemap.cs
+++ Advanced C# for Games -  Tilemapped Game Engine/Tilemap.cs
@@ -12,10 +12,65 @@ namespace Advanced_C__for_Games____Tilemapped_Game_Engine
         private string[,] _tiles; // should be built by Tile Class Objects
         private int _width;
         private int _height;
-
+        private List<TileObject> _actors;
+        private int _currentTurn;
 
         // need to create properties for all the private variables
-        
+
+        public int Width => _width;
+        public int Height => _height;
+        public List<TileObject> Actors => _actors;
+        public int CurrentTurn => _currentTurn;
+
+
+        public Tilemap(int width, int height)
+        {
+            _width = width;
+            _height = height;
+            _tiles = new Tile[height, width];
+            InitializeTiles();
+            _actors = new List<TileObject>();
+            _currentTurn = 0;
+        }
+
+        private void InitializeTiles()
+        {
+            for (int row = 0; row < _height; row++)
+            {
+                for (int col = 0; col < _width; col++)
+                {
+                    _tiles[row, col] = new Tile();
+                }
+            }
+        }
+
+        public void SetTileObject(int x, int y, TileObject tileObject)
+        {
+            if (IsValidPosition(x, y))
+            {
+                _tiles[y, x].OccupyingObject = tileObject;
+                tileObject.MoveToTile(new int[,] { { x, y } }, null);
+            }
+        }
+
+        public void AddActor(TileObject actor)
+        {
+            _actors.Add(actor);
+            Random rand = new Random();
+            int x, y;
+            do
+            {
+                x = rand.Next(Width);
+                y = rand.Next(Height);
+            } while (_tiles[y, x].OccupyingObject != null);
+            SetTileObject(x, y, actor);
+        }
+
+        public void HandleTurnOrder()
+        {
+            _currentTurn = (_currentTurn + 1) % _actors.Count;
+            Console.WriteLine($"It's now {_actors[_currentTurn].Name}'s turn.");
+        }
 
         public string GetTile(int x, int y)
         {
