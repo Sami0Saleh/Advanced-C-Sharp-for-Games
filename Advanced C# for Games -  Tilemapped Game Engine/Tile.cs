f269732950c438a50ff49b0109264716c671f793
Mansteel555 <denib79@gmail.com>
2024-05-25 16:27:32 +0300

    Changes to tilemaps

--- Advanced C# for Games -  Tilemapped Game Engine/Tile.cs
+++ Advanced C# for Games -  Tilemapped Game Engine/Tile.cs
@@ -6,71 +6,137 @@ using System.Threading.Tasks;
 
 namespace Advanced_C__for_Games____Tilemapped_Game_Engine
 {
-
- //   tile.Left, tile.Center, tile.Right, tile.ForegroundColor, tile.BackgroundColor
     public abstract class Tile : ICloneable
     {
+        public int ID { get; private set; }
+        public int[,] Position { get; private set; }
         public ConsoleColor _backgroundColor;
         public ConsoleColor _foregroundColor;
         public char Left;
         public char Center;
         public char Right;
-
-
-
         private Actor _actor; // the actor the tile belongs to 
-
         private TileObject _tileObject = null;
-        private string _tileValue = "  ";  // get from tile object obtaining it, if emptey is 2 spaces
-        // tile class should know what position hes in
-        private string actor;
+        private string _tileValue = "  ";  // get from tile object obtaining it, if empty is 2 spaces
+
         public string TileValue
         {
             get { return _tileValue; }
             set { _tileValue = value; }
         }
 
-        // Method to check if the tile is occupied
+        
+        public Tile(int id, int row, int col, string initialValue)
+        {
+            ID = id;
+            Position = new int[,] { { row, col } };
+            _tileValue = initialValue;
+        }
+
+        
         public bool IsOccupied()
-        {          
+        {
             return _tileValue != null;
         }
 
-        // Method to occupy the tile with a tile object
+        
         public bool OccupyTile()
         {
             if (!IsOccupied())
             {
-                _tileValue = "  "; // Arbitrarily assigning 1 to represent an occupied tile
+                _tileValue = "  "; 
                 return true;
             }
             else
             {
-                return false; // Tile is already occupied
+                return false; 
             }
         }
 
-        // Method to vacate the tile
+        
         public void VacateTile()
         {
-           
-            _tileValue = "  "; // Resetting the tile value to represent an unoccupied tile
+            _tileValue = "  ";
         }
-        
-    
 
         public void Notify(TileObject tileobject)
         {
-            if (true /* passing thorugh tiles*/ )
+            if (true )
             { }
-            if (true /* lands on tiles  tiles*/ )
+            if (true )
             { }
-        }
-        public Tile(string initialValue)
-        {
-            _tileValue = initialValue;
         }
 
         public abstract object Clone();
     }
-}
+
+
+
+
+ //   tile.Left, tile.Center, tile.Right, tile.ForegroundColor, tile.BackgroundColor
+ //    public abstract class Tile : ICloneable
+ //    {
+ //        public ConsoleColor _backgroundColor;
+ //        public ConsoleColor _foregroundColor;
+ //        public char Left;
+ //        public char Center;
+ //        public char Right;
+
+
+
+//        private Actor _actor; // the actor the tile belongs to 
+
+//        private TileObject _tileObject = null;
+//        private string _tileValue = "  ";  // get from tile object obtaining it, if emptey is 2 spaces
+//        // tile class should know what position hes in
+//        private string actor;
+//        public string TileValue
+//        {
+//            get { return _tileValue; }
+//            set { _tileValue = value; }
+//        }
+
+//        // Method to check if the tile is occupied
+//        public bool IsOccupied()
+//        {          
+//            return _tileValue != null;
+//        }
+
+//        // Method to occupy the tile with a tile object
+//        public bool OccupyTile()
+//        {
+//            if (!IsOccupied())
+//            {
+//                _tileValue = "  "; // Arbitrarily assigning 1 to represent an occupied tile
+//                return true;
+//            }
+//            else
+//            {
+//                return false; // Tile is already occupied
+//            }
+//        }
+
+//        // Method to vacate the tile
+//        public void VacateTile()
+//        {
+
+//            _tileValue = "  "; // Resetting the tile value to represent an unoccupied tile
+//        }
+
+
+
+//        public void Notify(TileObject tileobject)
+//        {
+//            if (true /* passing thorugh tiles*/ )
+//            { }
+//            if (true /* lands on tiles  tiles*/ )
+//            { }
+//        }
+//        public Tile(string initialValue)
+//        {
+//            _tileValue = initialValue;
+//        }
+
+//        public abstract object Clone();
+//    }
+//}
--- Advanced C# for Games -  Tilemapped Game Engine/Tilemap.cs
+++ Advanced C# for Games -  Tilemapped Game Engine/Tilemap.cs
@@ -10,34 +10,38 @@ using System.Diagnostics.CodeAnalysis;
 
 namespace Advanced_C__for_Games____Tilemapped_Game_Engine
 {
-    public abstract class Tilemap : IEnumerable<Tile>
+
+    public class Tilemap : IEnumerable<Tile>
     {
-        
-        public Tile[,] _tiles;
+        private Tile[,] _tiles;
         private int _mapWidth;
         private int _mapHeight;
-
-        // need to create properties for all the private variables
+        private List<Actor> _actors;
+        private Dictionary<int, Tile> _tileDictionary;
 
         public int Width => _mapWidth;
         public int Height => _mapHeight;
-
 
         public Tilemap(int width, int height)
         {
             _mapWidth = width;
             _mapHeight = height;
             _tiles = new Tile[height, width];
+            _actors = new List<Actor>();
+            _tileDictionary = new Dictionary<int, Tile>();
             InitializeTiles();
         }
 
         private void InitializeTiles()
         {
+            int idCounter = 0;
             for (int row = 0; row < _mapHeight; row++)
             {
                 for (int col = 0; col < _mapWidth; col++)
                 {
-                //    _tiles[row, col] = new Tile();
+                    var tile = new SimpleTile(idCounter++, row, col, "  ");
+                    _tiles[row, col] = tile;
+                    _tileDictionary.Add(tile.ID, tile);
                 }
             }
         }
@@ -46,41 +50,20 @@ namespace Advanced_C__for_Games____Tilemapped_Game_Engine
         {
             if (IsValidPosition(x, y))
             {
-              //  _tiles[y, x].OccupyingObject = tileObject;
-                tileObject.MoveToTile(new int[,] { { x, y } }, null);
+                var tile = (SimpleTile)_tiles[y, x];
+                tile.OccupyingObject = tileObject;
+                tileObject.MoveToTile(new int[,] { { x, y } }, tile.Position);
             }
         }
-
-        public void AddActor(TileObject actor)
-        {
-            // _actors.Add(actor);
-            Random rand = new Random();
-            int x, y;
-           /* do
-            {
-                x = rand.Next(Width);
-                y = rand.Next(Height);
-            }
-            while (_tiles[y, x].OccupyingObject != null);
-            SetTileObject(x, y, actor);*/
-        }
-
-       /* public void HandleTurnOrder()
-        {
-            _currentTurn = (_currentTurn + 1) % _actors.Count;
-            Console.WriteLine($"It's now {_actors[_currentTurn].Name}'s turn.");
-        }*/
 
         public Tile GetTile(int x, int y)
         {
-            if (IsValidPosition(x, y))
-            {
-                return _tiles[y, x];
-            }
-            else
-            {
-                return null; // need to return to user and let him enter new coordinates 
-            }
+            return IsValidPosition(x, y) ? _tiles[y, x] : null;
+        }
+
+        public Tile GetTileByID(int id)
+        {
+            return _tileDictionary.TryGetValue(id, out var tile) ? tile : null;
         }
 
         public void SetTile(int x, int y, Tile tileValue)
@@ -88,104 +71,272 @@ namespace Advanced_C__for_Games____Tilemapped_Game_Engine
             if (IsValidPosition(x, y))
             {
                 _tiles[y, x] = tileValue;
-            }
-            else
-            {
-                // what if a player tries to set in a unvalided position
+                _tileDictionary[tileValue.ID] = tileValue;
             }
         }
 
-        public void PrintTilemap() // need to become a set and put print into a visual class
+        public void MoveActor(Actor actor, int newX, int newY)
+        {
+            if (IsValidPosition(newX, newY))
+            {
+                var currentTile = GetTile(actor.Position[0, 0], actor.Position[0, 1]);
+                var newTile = GetTile(newX, newY);
+                if (newTile.IsOccupied())
+                {
+                    Console.WriteLine("Tile is already occupied.");
+                    return;
+                }
+
+                currentTile.VacateTile();
+                newTile.OccupyTile();
+                actor.Position = new int[,] { { newX, newY } };
+            }
+            else
+            {
+                Console.WriteLine("Invalid position.");
+            }
+        }
+
+        public void AddActor(Actor actor, int x, int y)
+        {
+            if (IsValidPosition(x, y) && !_tiles[y, x].IsOccupied())
+            {
+                _actors.Add(actor);
+                actor.Position = new int[,] { { x, y } };
+                _tiles[y, x].OccupyTile();
+            }
+            else
+            {
+                Console.WriteLine("Invalid position or tile is occupied.");
+            }
+        }
+
+        public void RemoveActor(Actor actor)
+        {
+            var tile = GetTile(actor.Position[0, 0], actor.Position[0, 1]);
+            if (tile != null && _actors.Contains(actor))
+            {
+                tile.VacateTile();
+                _actors.Remove(actor);
+            }
+        }
+
+        public void PrintTilemap()
         {
             for (int row = 0; row < _mapHeight; row++)
             {
                 for (int col = 0; col < _mapWidth; col++)
                 {
-                    ChessCheckersMapColor(_tiles[col,row], col, row);
-                    if ((row == 0 && (col != 0 && col != (_mapWidth - 1))) || (row == (_mapHeight - 1) && (col != 0 && col != (_mapWidth - 1))))
-                    {
-                        Console.Write("██"); //Row
-                    }
-                    else if (col == 0 || col == (_mapWidth - 1)) // Colum
-                    {
-                        Console.Write("██");
-                    }
-                    else if (row != 0 || col != 0)
-                    {
-                        Console.Write(_tiles[row, col] + "  ");
-                    }
+                    var tile = _tiles[row, col];
+                    Console.BackgroundColor = (col + row) % 2 == 0 ? ConsoleColor.White : ConsoleColor.Black;
+                    Console.Write(tile.TileValue);
+                    Console.ResetColor();
                 }
                 Console.WriteLine();
             }
-
         }
+
         private bool IsValidPosition(int x, int y)
         {
             return x >= 0 && x < _mapWidth && y >= 0 && y < _mapHeight;
         }
 
-
-
-
-        // should all be moved to a color class
-        private void ColorTile(ConsoleColor color, int col, int row)
-        {
-            Console.BackgroundColor = color;
-        }
-
-        private void ChessCheckersMapColor(Tile tile, int col, int row)
-        {
-            if ((col + row) % 2 != 0)
-            {
-
-                ColorTile(ConsoleColor.White, col, row);
-            }
-            else
-            {
-                ColorTile(ConsoleColor.Black, col, row);
-            }
-        }
-
-
-        /// IEnumerble and IEnumrator
-        ///  
-        ///  
-        /// 
-
         public IEnumerator<Tile> GetEnumerator()
         {
-            return null;
+            for (int row = 0; row < _mapHeight; row++)
+            {
+                for (int col = 0; col < _mapWidth; col++)
+                {
+                    yield return _tiles[row, col];
+                }
+            }
         }
 
         IEnumerator IEnumerable.GetEnumerator()
         {
-            return this.GetEnumerator();
-        }
-
-        /// 
-        ///  2D Position Interface 
-        /// 
-        public struct IPosition
-        {
-            // needs to override Must override ToString(), GetHashCode(), and Equals()
-         
-            public override string ToString()
-            {
-                return base.ToString();
-            }
-            public override int GetHashCode()
-            { 
-               return base.GetHashCode();
-            }
-            public override bool Equals([NotNullWhen(true)] object? obj)
-            {
-                return base.Equals(obj);
-            }
-
-            // Must override operators for addition and subtraction(+, -)
-
-
-
+            return GetEnumerator();
         }
     }
+
+
+
+
+    //public abstract class Tilemap : IEnumerable<Tile>
+    //{
+
+    //    public Tile[,] _tiles;
+    //    private int _mapWidth;
+    //    private int _mapHeight;
+
+    //    // need to create properties for all the private variables
+
+    //    public int Width => _mapWidth;
+    //    public int Height => _mapHeight;
+
+
+    //    public Tilemap(int width, int height)
+    //    {
+    //        _mapWidth = width;
+    //        _mapHeight = height;
+    //        _tiles = new Tile[height, width];
+    //        InitializeTiles();
+    //    }
+
+    //    private void InitializeTiles()
+    //    {
+    //        for (int row = 0; row < _mapHeight; row++)
+    //        {
+    //            for (int col = 0; col < _mapWidth; col++)
+    //            {
+    //            //    _tiles[row, col] = new Tile();
+    //            }
+    //        }
+    //    }
+
+    //    public void SetTileObject(int x, int y, TileObject tileObject)
+    //    {
+    //        if (IsValidPosition(x, y))
+    //        {
+    //          //  _tiles[y, x].OccupyingObject = tileObject;
+    //            tileObject.MoveToTile(new int[,] { { x, y } }, null);
+    //        }
+    //    }
+
+    //    public void AddActor(TileObject actor)
+    //    {
+    //        // _actors.Add(actor);
+    //        Random rand = new Random();
+    //        int x, y;
+    //       /* do
+    //        {
+    //            x = rand.Next(Width);
+    //            y = rand.Next(Height);
+    //        }
+    //        while (_tiles[y, x].OccupyingObject != null);
+    //        SetTileObject(x, y, actor);*/
+    //    }
+
+    //   /* public void HandleTurnOrder()
+    //    {
+    //        _currentTurn = (_currentTurn + 1) % _actors.Count;
+    //        Console.WriteLine($"It's now {_actors[_currentTurn].Name}'s turn.");
+    //    }*/
+
+    //    public Tile GetTile(int x, int y)
+    //    {
+    //        if (IsValidPosition(x, y))
+    //        {
+    //            return _tiles[y, x];
+    //        }
+    //        else
+    //        {
+    //            return null; // need to return to user and let him enter new coordinates 
+    //        }
+    //    }
+
+    //    public void SetTile(int x, int y, Tile tileValue)
+    //    {
+    //        if (IsValidPosition(x, y))
+    //        {
+    //            _tiles[y, x] = tileValue;
+    //        }
+    //        else
+    //        {
+    //            // what if a player tries to set in a unvalided position
+    //        }
+    //    }
+
+    //    public void PrintTilemap() // need to become a set and put print into a visual class
+    //    {
+    //        for (int row = 0; row < _mapHeight; row++)
+    //        {
+    //            for (int col = 0; col < _mapWidth; col++)
+    //            {
+    //                ChessCheckersMapColor(_tiles[col,row], col, row);
+    //                if ((row == 0 && (col != 0 && col != (_mapWidth - 1))) || (row == (_mapHeight - 1) && (col != 0 && col != (_mapWidth - 1))))
+    //                {
+    //                    Console.Write("██"); //Row
+    //                }
+    //                else if (col == 0 || col == (_mapWidth - 1)) // Colum
+    //                {
+    //                    Console.Write("██");
+    //                }
+    //                else if (row != 0 || col != 0)
+    //                {
+    //                    Console.Write(_tiles[row, col] + "  ");
+    //                }
+    //            }
+    //            Console.WriteLine();
+    //        }
+
+    //    }
+    //    private bool IsValidPosition(int x, int y)
+    //    {
+    //        return x >= 0 && x < _mapWidth && y >= 0 && y < _mapHeight;
+    //    }
+
+
+
+
+    //    // should all be moved to a color class
+    //    private void ColorTile(ConsoleColor color, int col, int row)
+    //    {
+    //        Console.BackgroundColor = color;
+    //    }
+
+    //    private void ChessCheckersMapColor(Tile tile, int col, int row)
+    //    {
+    //        if ((col + row) % 2 != 0)
+    //        {
+
+    //            ColorTile(ConsoleColor.White, col, row);
+    //        }
+    //        else
+    //        {
+    //            ColorTile(ConsoleColor.Black, col, row);
+    //        }
+    //    }
+
+
+    //    /// IEnumerble and IEnumrator
+    //    ///  
+    //    ///  
+    //    /// 
+
+    //    public IEnumerator<Tile> GetEnumerator()
+    //    {
+    //        return null;
+    //    }
+
+    //    IEnumerator IEnumerable.GetEnumerator()
+    //    {
+    //        return this.GetEnumerator();
+    //    }
+
+    //    /// 
+    //    ///  2D Position Interface 
+    //    /// 
+    //    public struct IPosition
+    //    {
+    //        // needs to override Must override ToString(), GetHashCode(), and Equals()
+
+    //        public override string ToString()
+    //        {
+    //            return base.ToString();
+    //        }
+    //        public override int GetHashCode()
+    //        { 
+    //           return base.GetHashCode();
+    //        }
+    //        public override bool Equals([NotNullWhen(true)] object? obj)
+    //        {
+    //            return base.Equals(obj);
+    //        }
+
+    //        // Must override operators for addition and subtraction(+, -)
+
+
+
+    //    }
+    //}
 }
